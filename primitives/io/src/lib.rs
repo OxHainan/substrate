7b3bb984ff7a5e11fb4257e44b11c2706f85a8f9
JUN <yingjun@oxhainan.org>
2023-04-21 16:27:20 +0800

    add aes gcm

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -67,9 +67,16 @@ use sp_runtime_interface::{
 use codec::{Decode, Encode};
 
 #[cfg(feature = "std")]
+use aes_gcm::{
+	aead::{AeadMut, Payload},
+	Aes256Gcm, KeyInit, Nonce,
+};
+
+#[cfg(feature = "std")]
 use secp256k1::{
+	ecdh::SharedSecret,
 	ecdsa::{RecoverableSignature, RecoveryId},
-	Message, SECP256K1,
+	Message, PublicKey, SecretKey, SECP256K1,
 };
 
 #[cfg(feature = "std")]
@@ -95,6 +102,31 @@ pub enum EcdsaVerifyError {
 	BadV,
 	/// Invalid signature
 	BadSignature,
+
+	/// Invalid  public key
+	BadPublicKey,
+
+	/// Invalid secret key
+	BadSecretKey,
+
+	/// Invalid shared secret
+	BadSharedSecret,
+}
+
+/// Error aes gcm
+#[derive(Encode, Decode)]
+pub enum AesError {
+	/// Bad shard key
+	BadSharedKey,
+
+	/// Bad shared key length
+	BadSharedKeyLength,
+
+	/// Bad encrypted
+	BadEncrypted,
+
+	/// Bad decrypted
+	BadDecrypted,
 }
 
 /// The outcome of calling `storage_kill`. Returned value is the number of storage items
@@ -1026,6 +1058,40 @@ pub trait Crypto {
 			.unwrap_or_else(|| ecdsa_verify(sig, msg, pub_key))
 	}
 
+	/// encrypted
+	fn encrypted(msg: &[u8], aad: &[u8; 32], pubkey: &[u8; 64]) -> Result<Vec<u8>, AesError> {
+		let shared_key = shared_secret(pubkey).map_err(|_| AesError::BadSharedKey)?;
+		Aes256Gcm::new_from_slice(&shared_key)
+			.map_err(|_| AesError::BadSharedKeyLength)?
+			.encrypt(Nonce::from_slice(&aad[20..]), Payload { aad, msg })
+			.map_err(|_| AesError::BadEncrypted)
+	}
+
+	/// decrypted
+	fn decrypted(msg: &[u8], aad: &[u8; 32], pubkey: &[u8; 64]) -> Result<Vec<u8>, AesError> {
+		let shared_key = shared_secret(pubkey).map_err(|_| AesError::BadSharedKey)?;
+		Aes256Gcm::new_from_slice(&shared_key)
+			.map_err(|_| AesError::BadSharedKeyLength)?
+			.decrypt(Nonce::from_slice(&aad[20..]), Payload { aad, msg })
+			.map_err(|_| AesError::BadDecrypted)
+	}
+
+	/// ecdh
+	fn shared_secret(pubkey: &[u8; 64]) -> Result<[u8; 32], EcdsaVerifyError> {
+		let mut tagged_full = [0u8; 65];
+		tagged_full[0] = 0x04;
+		tagged_full[1..].copy_from_slice(pubkey);
+		let sk = <SecretKey as core::str::FromStr>::from_str(
+			"57f0148f94d13095cfda539d0da0d1541304b678d8b36e243980aab4e1b7cead",
+		)
+		.map_err(|_| EcdsaVerifyError::BadSecretKey)?;
+		Ok(SharedSecret::new(
+			&PublicKey::from_slice(&tagged_full).map_err(|_| EcdsaVerifyError::BadPublicKey)?,
+			&sk,
+		)
+		.secret_bytes())
+	}
+
 	/// Verify and recover a SECP256k1 ECDSA signature.
 	///
 	/// - `sig` is passed in RSV format. V should be either `0/1` or `27/28`.
@@ -1731,6 +1797,27 @@ mod tests {
 			assert_eq!(storage::get(b"hello"), None);
 			assert_eq!(storage::get(b"foo00"), Some(value.clone().into()));
 		});
+	}
+
+	#[test]
+	fn test_aes_gcm() {
+		let pk = array_bytes::hex2array_unchecked("8db55b05db86c0b1786ca49f095d76344c9e6056b2f02701a7e7f3c20aabfd913ebbe148dd17c56551a52952371071a6c604b3f3abe8f2c8fa742158ea6dd7d4");
+
+		assert!(crypto::shared_secret(&pk).is_ok());
+
+		let msg = b"hello world";
+		let aad = hashing::keccak_256(msg);
+		let ciphertext = match crypto::encrypted(msg, &aad, &pk) {
+			Ok(cipher) => cipher,
+			Err(_) => Default::default(),
+		};
+
+		let plaintext = match crypto::decrypted(&ciphertext, &aad, &pk) {
+			Ok(plain) => plain,
+			Err(_) => Default::default(),
+		};
+
+		assert_eq!(&plaintext, msg);
 	}
 
 	#[test]
